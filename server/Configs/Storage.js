2b0d2c2fb509fb1cecdf92df8cde80ce185a2a00
AHMED HASSAN <151830489+AHMED1CB@users.noreply.github.com>
2025-10-19 12:13:15 +0300

    allow Making Nested Directories

--- server/Configs/Storage.js
+++ server/Configs/Storage.js
@@ -7,20 +7,20 @@ import fs from 'fs';
 const storagePath = path.join(process.cwd(), "Storage/photos");
 
 
-if (!fs.existsSync(storagePath)){
-    fs.mkdirSync(storagePath)
+if (!fs.existsSync(storagePath)) {
+    fs.mkdirSync(storagePath, { recursive: true })
 }
 
 const userPhotosStorage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null , storagePath)
+    destination: (req, file, cb) => {
+        cb(null, storagePath)
     },
-    filename: (req,file,cb) => {
+    filename: (req, file, cb) => {
         const extension = path.extname(file.originalname)
-        const name =  'user-img' + "-" + Date.now() + randomInt(9999) + extension ; 
-        cb(null,name)
+        const name = 'user-img' + "-" + Date.now() + randomInt(9999) + extension;
+        cb(null, name)
     }
 });
- 
 
-export const uploadPhoto = multer({storage : userPhotosStorage})
+
+export const uploadPhoto = multer({ storage: userPhotosStorage })
